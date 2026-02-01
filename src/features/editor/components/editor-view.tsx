454b8e4cc21f3c5f3a8840c941d47c89e282145c
Nityam Suchak <nityamsuchak@gmail.com>
2026-02-01 14:30:01 +0530

    11: AI Features

--- src/features/editor/components/editor-view.tsx
+++ src/features/editor/components/editor-view.tsx
@@ -5,7 +5,7 @@ import { FileBreadcrumbs } from "./file-breadcrumbs";
 import { TopNavigation } from "./top-navigation";
 import Image from "next/image";
 import { CodeEditor } from "./code-editor";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const DEBOUNCE_MS = 1500;
 
@@ -17,6 +17,15 @@ export const EditorView = ({ projectId }: { projectId: Id<"projects"> }) => {
 
   const isActiveFileBinary = activeFile && activeFile.storageId;
   const isActiveFileText = activeFile && !activeFile.storageId;
+
+  // Cleanup pending debounced updates on unmount or file change
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [activeTabId]);
 
   return (
     <div className="h-full flex flex-col">
@@ -52,9 +61,7 @@ export const EditorView = ({ projectId }: { projectId: Id<"projects"> }) => {
             fileName={activeFile.name}
           />
         )}
-        {isActiveFileBinary && (
-            <p>TODO: Implement binary preview</p>
-        )}
+        {isActiveFileBinary && <p>TODO: Implement binary preview</p>}
       </div>
     </div>
   );
