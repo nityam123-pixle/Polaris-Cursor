bf496708172a1407200d2d94a2032b9e6b104745
Nityam Suchak <nityamsuchak@gmail.com>
2026-03-12 15:26:41 +0530

    14: Webcontainers, terminal & preview

--- src/features/projects/components/project-id-view.tsx
+++ src/features/projects/components/project-id-view.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FileExplorer } from "./file explorer";
 import { EditorView } from "@/features/editor/components/editor-view";
+import { PreviewView } from "./preview-view";
 
 const MIN_SIDEBAR_WIDTH = 200;
 const MAX_SIDEBAR_WIDTH = 800;
@@ -140,7 +141,7 @@ export const ProjectIdView = ({ projectId }: { projectId: Id<"projects"> }) => {
             activeView === "preview" ? "visible" : "invisible"
           )}
         >
-          <div>Preview!</div>
+          <PreviewView projectId={projectId} />
         </div>
       </div>
     </div>
